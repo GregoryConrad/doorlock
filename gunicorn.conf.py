45e2168a511348e4183bc1ff95adb0c9ce385747
GregoryConrad <gregorysconrad@gmail.com>
2022-01-10 18:52:14 -0500

    Fixed name conflict

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,10 +1,10 @@
 # See https://docs.gunicorn.org/en/stable/settings.html#settings
 
 import multiprocessing
-from doorlock_config import config
+from doorlock_config import config as doorlock_config
 
 
-bind = f"0.0.0.0:{config['serverPort']}"
+bind = f"0.0.0.0:{doorlock_config['serverPort']}"
 workers = multiprocessing.cpu_count() * 2 + 1
-certfile = config['certfile']
-keyfile = config['keyfile']
+certfile = doorlock_config['certfile']
+keyfile = doorlock_config['keyfile']
