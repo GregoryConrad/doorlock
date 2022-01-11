5f9ac3280db589a580b55870463ae79362cb1d12
GregoryConrad <gregorysconrad@gmail.com>
2022-01-10 19:28:36 -0500

    Fixed reference bug

--- doorlock_config.py
+++ doorlock_config.py
@@ -12,7 +12,8 @@ def get_file(filename):
 def refresh_config():
     global config
     with open(get_file(config_filename), 'r') as config_file:
-        config = json.load(config_file)
+        for key, value in json.load(config_file).items():
+            config[key] = value
 
 
 def update_config(new_config):
