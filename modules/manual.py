1e59a5aebc5025e46edba7b2ae984b1fb554eee8
GregoryConrad <gregorysconrad@gmail.com>
2022-01-16 19:58:57 -0500

    Updated manual module to use new config

--- modules/manual.py
+++ modules/manual.py
@@ -1,12 +1,12 @@
 from gpiozero import Button
-from config.doorlock import config
+from config.doorlock import lock_button_pin, unlock_button_pin
 from modules.controller import lock, unlock
 from time import sleep
 
 if __name__ == "__main__":
-    lock_btn = Button(config['lockPin'])
+    lock_btn = Button(lock_button_pin)
     lock_btn.when_released = lock
-    unlock_btn = Button(config['unlockPin'])
+    unlock_btn = Button(unlock_button_pin)
     unlock_btn.when_released = unlock
 
     while True:
