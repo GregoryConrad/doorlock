86552b9718b9dbd3096bbbbdbad1a3c04f72193a
GregoryConrad <gregorysconrad@gmail.com>
2022-01-16 19:52:57 -0500

    added bug fix for config file

--- config/doorlock.py
+++ config/doorlock.py
@@ -8,7 +8,7 @@ from base64 import b64encode, b64decode
 
 
 def get_config_file(filename):
-    Path(__file__).parent / filename
+    return Path(__file__).parent / filename
 
 
 def get_key(filename):
